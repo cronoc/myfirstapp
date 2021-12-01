d6a27a0c594826ab8ac99ae539aa635980aa8ed8
cronoc <cronoc@yandex.ru>
2021-12-02 01:46:22 +0800

    dz1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreateПШ(savedInstanceState: Bundle?) {
+
+    lateinit var helloTextView: TextView
+    lateinit var newTextButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        helloTextView = findViewById(R.id.up_text_view)
+        newTextButton = findViewById(R.id.center_button)
+        newTextButtinClick()
     }
+
+    fun newTextButtinClick() {
+        newTextButton.setOnClickListener {
+           // helloTextView.text = Random.nextInt().toString()
+            Toast.makeText(this,"Hello"+Random.nextInt().toString(),Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
